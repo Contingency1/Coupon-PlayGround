c900737ea0683a54b7f88ef6d6f95f5ca48c7ac3
JYE <jo1814@naver.com>
2026-04-22 17:17:20 +0900

    init

---
+++ src/main/java/com/couponplayground/api/CouponApiApplication.java
@@ -0,0 +1,13 @@
+package com.couponplayground.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CouponApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CouponApiApplication.class, args);
+	}
+
+}
